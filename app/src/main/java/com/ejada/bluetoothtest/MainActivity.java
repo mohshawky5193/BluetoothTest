bac1bff4ea27516a0cb3f47ceb594c261fef8ae3
Mohamed <mam2210_@hotmail.com>
2020-04-23 14:31:24 +0200

    initial commit

---
+++ app/src/main/java/com/ejada/bluetoothtest/MainActivity.java
@@ -0,0 +1,103 @@
+package com.ejada.bluetoothtest;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ejada.gurux.DLMSMeter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity{
+
+    TextView mTextView;
+    BluetoothSPP bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+
+        bt = new BluetoothSPP(this);
+
+        if(!bt.isBluetoothAvailable()) {
+            Toast.makeText(this,"Bluetooth not supported",Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            @Override
+            public void onDeviceConnected(String name, String address) {
+                mTextView.setText("Conntected to device Name:"+ name+" MAC Address:"+address);
+                String[] args = new String[]{"-S","the com port here"};
+                try {
+                    DLMSMeter.readMeter(args);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    mTextView.setText(e.getMessage());
+                }
+            }
+
+            @Override
+            public void onDeviceDisconnected() {
+
+            }
+
+            @Override
+            public void onDeviceConnectionFailed() {
+                mTextView.setText("Failed to connect to device");
+            }
+        });
+
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        if(!bt.isBluetoothEnabled()) {
+            Toast.makeText(this,"Please enable the bluetooth",Toast.LENGTH_LONG).show();
+            finish();
+        } else {
+            bt.startService(BluetoothState.DEVICE_OTHER);
+            Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if(resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/ejada/gurux/DLMSMeter.java
@@ -0,0 +1,98 @@
+package com.ejada.gurux;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import gurux.dlms.enums.ObjectType;
+import gurux.dlms.objects.GXDLMSObjectCollection;
+import gurux.io.BaudRate;
+import gurux.io.Parity;
+import gurux.io.StopBits;
+import gurux.net.GXNet;
+import gurux.serial.GXSerial;
+
+public class DLMSMeter {
+
+    public static void readMeter(String[] args)
+            throws IOException {
+        Settings settings = new Settings();
+
+        GXDLMSReader reader = null;
+        try {
+            ////////////////////////////////////////
+            // Handle command line parameters.
+            int ret = Settings.getParameters(args, settings);
+            if (ret != 0) {
+                System.exit(1);
+                return;
+            }
+
+            ////////////////////////////////////////
+            // Initialize connection settings.
+            if (settings.media instanceof GXSerial) {
+                GXSerial serial = (GXSerial) settings.media;
+                if (settings.iec) {
+                    serial.setBaudRate(BaudRate.BAUD_RATE_300);
+                    serial.setDataBits(7);
+                    serial.setParity(Parity.EVEN);
+                    serial.setStopBits(StopBits.ONE);
+                } else {
+                    serial.setBaudRate(BaudRate.BAUD_RATE_9600);
+                    serial.setDataBits(8);
+                    serial.setParity(Parity.NONE);
+                    serial.setStopBits(StopBits.ONE);
+                }
+            } else if (settings.media instanceof GXNet) {
+            } else {
+                throw new Exception("Unknown media type.");
+            }
+            ////////////////////////////////////////
+            reader = new GXDLMSReader(settings.client, settings.media,
+                    settings.trace, settings.iec, settings.invocationCounter);
+            settings.media.open();
+            if (!settings.readObjects.isEmpty()) {
+                reader.initializeConnection();
+                if (settings.outputFile != null
+                        && new File(settings.outputFile).exists()) {
+                    try {
+                        GXDLMSObjectCollection c = GXDLMSObjectCollection
+                                .load(settings.outputFile);
+                        settings.client.getObjects().addAll(c);
+                    } catch (Exception ex) {
+                        // It's OK if this fails.
+                        System.out.print(ex.getMessage());
+                    }
+                } else {
+                    reader.getAssociationView();
+                    if (settings.outputFile != null) {
+                        settings.client.getObjects().save(settings.outputFile,
+                                null);
+                    }
+                }
+                for (Map.Entry<String, Integer> it : settings.readObjects) {
+                    Object val = reader.read(
+                            settings.client.getObjects()
+                                    .findByLN(ObjectType.NONE, it.getKey()),
+                            it.getValue());
+                    reader.showValue(it.getValue(), val);
+                }
+            } else {
+                reader.readAll(settings.outputFile);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            System.exit(1);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    System.exit(1);
+                }
+            }
+            System.out.println("Ended. Press any key to continue.");
+        }
+    }
+}
